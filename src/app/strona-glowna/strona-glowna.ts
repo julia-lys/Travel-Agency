f42faa9266abf4a354ee795b748f8c88b3c41f7b
julia-lys <julia.lyszkowska@gmail.com>
2025-12-17 14:51:26 +0100

    feat: implement best travel offers on the main page

--- src/app/strona-glowna/strona-glowna.ts
+++ src/app/strona-glowna/strona-glowna.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { KRAJE, TYPY_WAKACJI } from '../destynacje.data';
+import { OFERTY, Oferta } from '../oferty/oferty.data';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-strona-glowna',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './strona-glowna.html',
   styleUrl: './strona-glowna.css',
 })
 export class StronaGlowna {
+  najlepszeOferty: Oferta[] =[]
+  
+  ngOnInit() {
+    this.najlepszeOferty = OFERTY.slice(0, 3);
+  }
+
   listaKrajow = KRAJE;
   listaTypow = TYPY_WAKACJI;
 }
